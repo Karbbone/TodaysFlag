8090414a98e2cc66b2e9854783b0ec8a1247552d
Karbbone56 <clement.maillet56@gmail.com>
2024-03-21 15:45:46 +0100

    migrataion to vite

--- src/components/Flag.jsx
+++ src/components/Flag.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 function Flag(props){
     return(
+        // eslint-disable-next-line react/prop-types
         <img alt='Flag' className="flag" style={{ height: "250px", width: "400px" }} src={props.flagUrl}></img>
     )
 }
