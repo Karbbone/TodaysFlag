73cd0988042c48ed1a04095e2e75819a48ebef3b
Karbbone56 <clement.maillet56@gmail.com>
2024-06-06 19:12:53 +0200

    put swipper in a flex with text

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,14 @@
 import logo from "../assets/freepik/20944707.png";
 import backgroundFlagGame from "../assets/freepik/red-blue-polygonal-background/O4YIH10.jpg";
 import Paris from "../assets/paris.jpg";
-import Sprint from "../assets/freepik/flat-design-cross-country-illustration/7461474.jpg";
+import Sprint from "../assets/freepik/flat-design-cross-country-illustration/7461474.jpg"; // Import Swiper React components
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 export const Homepage = () => {
   return (
     <>
@@ -27,7 +34,7 @@ export const Homepage = () => {
       <div className="separator"></div>
       <section id="section-mode">
         <div className="wrapper">
-          <h2>
+          <h2 className="title-section-home-page">
             Modes de <span className="span-gradient">jeux</span>
           </h2>
           <div className="games-list">
@@ -62,6 +69,46 @@ export const Homepage = () => {
         </div>
       </section>
       <div className="separator"></div>
+      <section id="section-flag-description">
+        <div className="wrapper">
+          <h2 className="title-section-home-page">
+            Mode <span className="span-gradient">Drapeau</span>
+          </h2>
+          <div className="content">
+            <div className="text">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus quibusdam nisi illo obcaecati sint quas odit! Hic
+                dolorum ipsum voluptatem reiciendis incidunt, cupiditate ducimus
+                temporibus fuga dolorem, tempora, aperiam vero.
+              </p>
+            </div>
+            <div className="swiper-container">
+              <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+              >
+                <SwiperSlide>
+                  <img src={Paris}></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={Sprint}></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={logo}></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={Paris}></img>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
