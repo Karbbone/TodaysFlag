dfa84f73dbd63f0ffb5664d4057b898e40469c61
Karbbone56 <clement.maillet56@gmail.com>
2024-06-24 17:29:55 +0200

    homepage not ended

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,18 @@ import logo from "../assets/freepik/20944707.png";
 import backgroundFlagGame from "../assets/freepik/red-blue-polygonal-background/O4YIH10.jpg";
 import Paris from "../assets/paris.jpg";
 import Sprint from "../assets/freepik/flat-design-cross-country-illustration/7461474.jpg"; // Import Swiper React components
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 
 export const Homepage = () => {
   return (
@@ -77,34 +83,89 @@ export const Homepage = () => {
           <div className="content">
             <div className="text">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Doloribus quibusdam nisi illo obcaecati sint quas odit! Hic
-                dolorum ipsum voluptatem reiciendis incidunt, cupiditate ducimus
-                temporibus fuga dolorem, tempora, aperiam vero.
+                Bienvenue, voici le premier mode de jeu original de Today&apos;s
+                flag !
               </p>
+              <p>
+                Voici la règle, elle est très simple : Chaque jour, un nouveau
+                drapeau vous sera présenté, et votre mission est de deviner le
+                pays correspondant. Les espaces dans le nom du pays seront
+                indiqués dans la légende.
+              </p>
+              <p>
+                Amusez-vous bien et testez vos connaissances en géographie tout
+                en vous amusant !
+              </p>
+              <span className="span-gradient">ENJOY !</span>
             </div>
             <div className="swiper-container">
               <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
                 spaceBetween={50}
                 slidesPerView={1}
                 navigation
                 pagination={{ clickable: true }}
                 scrollbar={{ draggable: true }}
+                autoplay={{ delay: 2500, disableOnInteraction: false }}
               >
                 <SwiperSlide>
-                  <img src={Paris}></img>
+                  <img src="https://flagcdn.com/w320/fr.png"></img>
                 </SwiperSlide>
                 <SwiperSlide>
-                  <img src={Sprint}></img>
+                  <img src="https://flagcdn.com/w320/jp.png"></img>
                 </SwiperSlide>
                 <SwiperSlide>
-                  <img src={logo}></img>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <img src={Paris}></img>
+                  <img src="https://flagcdn.com/w320/it.png"></img>
                 </SwiperSlide>
               </Swiper>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="separator"></div>
+      <section id="section-flag-description">
+        <div className="wrapper">
+          <h2 className="title-section-home-page">
+            Mode <span className="span-gradient">Capitale</span>
+          </h2>
+          <div className="content">
+            <div className="swiper-container">
+              <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                autoplay={{ delay: 2500, disableOnInteraction: false }}
+              >
+                <SwiperSlide>
+                  <img src="https://flagcdn.com/w320/fr.png"></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://flagcdn.com/w320/jp.png"></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://flagcdn.com/w320/it.png"></img>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+            <div className="text">
+              <p>
+                Bienvenue, voici le premier mode de jeu original de Today&apos;s
+                flag !
+              </p>
+              <p>
+                Voici la règle, elle est très simple : Chaque jour, un nouveau
+                drapeau vous sera présenté, et votre mission est de deviner le
+                pays correspondant. Les espaces dans le nom du pays seront
+                indiqués dans la légende.
+              </p>
+              <p>
+                Amusez-vous bien et testez vos connaissances en géographie tout
+                en vous amusant !
+              </p>
+              <span className="span-gradient">ENJOY !</span>
             </div>
           </div>
         </div>
