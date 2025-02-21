14a4643d57f6287db04172afab810016db34b4f1
Karbbone <clement.maillet56@gmail.com>
2025-02-21 15:42:57 +0100

    chore: remove old project

--- todays-flag/app/page.tsx
+++ todays-flag/app/page.tsx
@@ -1,4 +1,4 @@
-import { GameSection } from "./components/GameSection";
+// import { GameSection } from "./components/GameSection";
 import { HeroSection } from "./components/HeroSection";
 
 export default async function Home() {
