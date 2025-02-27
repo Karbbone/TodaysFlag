a8ad3664c2f3e36d32a983aab2534e8fd2940b6a
Karbbone <clement.maillet56@gmail.com>
2025-02-27 22:31:17 +0100

    feat(HeroSection): add button to /flag

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,9 +1,12 @@
 import { Globe } from "@/components/magicui/globe";
 import { AvatarAnimatedTooltip } from "@/components/ui/avatar-animated-tooltip";
+import { Button } from "@/components/ui/button";
+import { Flag } from "lucide-react";
+import Link from "next/link";
 
 export function HeroSection() {
   return (
-    <section className="pt-14 md:pt-28 min-h-[calc(100vh-20% )] border-b-[1px] border-ring overflow-hidden mx-auto px-4 flex relative">
+    <section className="pt-14 md:pt-28 min-h-[calc(100vh-75px)] border-b-[1px] border-ring overflow-hidden mx-auto px-4 flex relative">
       <div className="blur-circle pointer-events-none"></div>
       <div className="flex flex-col items-center justify-between gap-8 w-full relative z-10">
         <div className="w-full flex justify-center">
@@ -27,6 +30,16 @@ export function HeroSection() {
             <span>Apprenez les drapeaux du monde entier en vous amusant.</span>
           </p>
         </div>
+        <Link href="/flag">
+          <Button
+            className="flex items-center justify-center text-lg gap-2"
+            variant={"outline"}
+            size={"lg"}
+          >
+            <Flag className="h-10 w-10" />
+            <span>Devine le drapeau du jour</span>
+          </Button>
+        </Link>
         <div className="w-full flex justify-center overflow-hidden h-[250px]">
           <Globe />
         </div>
