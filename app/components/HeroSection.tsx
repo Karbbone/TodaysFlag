cd8596d84dd647a7435c2db032b4da8cffa66f30
Karbbone <clement.maillet56@gmail.com>
2025-02-27 21:35:46 +0100

    style: title modification hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -2,17 +2,21 @@ import { Globe } from "@/components/magicui/globe";
 
 export function HeroSection() {
   return (
-    <section className="pt-8 md:pt-16 min-h-[calc(100vh-20% )] border-b-[1px] border-ring overflow-hidden mx-auto px-4 flex relative">
+    <section className="pt-14 md:pt-28 min-h-[calc(100vh-20% )] border-b-[1px] border-ring overflow-hidden mx-auto px-4 flex relative">
       <div className="blur-circle"></div>
       <div className="flex flex-col items-center justify-between gap-16 w-full">
         <div>
-          <h2 className="text-7xl font-bold mb-4">
+          <h1 className="text-6xl font-bold mb-4 text-center">
             Today&apos;s{" "}
             <span className="bg-gradient-to-r from-flash to-primary bg-clip-text text-transparent">
               Flag
-            </span>
-          </h2>
-          <p className="text-2xl">
+            </span>{" "}
+            {""}
+            un défi quotidien.
+            <br />
+            Deviens un as de la géographie
+          </h1>
+          <p className="text-2xl text-center">
             Votre besoin quotidien de{" "}
             <span className="bg-gradient-to-r from-flash to-primary bg-clip-text text-transparent">
               culture générale
